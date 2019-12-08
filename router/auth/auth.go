f04c5f72ca8b522e3811d8d3a16dd6d9d534f3ed
Fedor Katurov <gotham48@gmail.com>
2019-12-08 20:53:39 +0700

    fixed typos

--- router/auth/auth.go
+++ router/auth/auth.go
@@ -2,7 +2,6 @@ package auth
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/NYTimes/gziphandler"
@@ -11,14 +10,8 @@ import (
 	"github.com/muerwre/orchidgo/logger"
 )
 
-// type AuthRouter struct {
-// 	Router *mux.Router
-// }
-
 // Router creates new AuthRouter
 func Router(router *mux.Router, logger *logger.Logger) {
-	fmt.Println(router, logger)
-
 	router.Handle("/second", gziphandler.GzipHandler(logger.Log(TestHandler)))
 }
 
@@ -31,10 +24,12 @@ func TestHandler(ctx *app.Context, w http.ResponseWriter, r *http.Request) error
 	http.Error(w, string(res), 401)
 
 	return nil
+}
 
+/*
 	// err := json.NewEncoder(w).Encode(map[string]string{
 	// 	"test": "Hello! Its working?",
 	// })
 
 	// return err
-}
+*/
