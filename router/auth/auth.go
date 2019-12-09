f7725ae565f55fb717ecdcaad9db622b2a238a93
Fedor Katurov <gotham48@gmail.com>
2019-12-09 15:07:29 +0700

    returning proper data structure

--- router/auth/auth.go
+++ router/auth/auth.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
 
 	"github.com/NYTimes/gziphandler"
@@ -19,18 +20,25 @@ func Router(router *mux.Router, logger *logger.Logger) {
 // CheckCredentials checks id and token and returns guest token if they're incorrect
 func CheckCredentials(ctx *app.Context, w http.ResponseWriter, r *http.Request) error {
 	user, err := ctx.DB.AssumeUserExist(r.URL.Query()["id"][0], r.URL.Query()["token"][0])
+	error := ""
 
 	if err != nil {
 		user = ctx.DB.GenerateGuestUser()
+		error = "User not found, falling back to guest"
 		ctx.DB.Create(&user)
 	}
 
 	random_url := ctx.DB.GenerateRandomUrl()
 
+	if user == nil || random_url == "" {
+		return errors.New("Failed to create reandom sequence")
+	}
+
 	err = json.NewEncoder(w).Encode(struct {
-		User      *model.User `json:"user"`
-		RandomUrl string      `json:"random_url"`
-	}{User: user, RandomUrl: random_url})
+		*model.User
+		RandomUrl string `json:"random_url"`
+		Error     string `json:"error"`
+	}{User: user, RandomUrl: random_url, Error: error})
 
 	if err != nil {
 		return err
