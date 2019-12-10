966f120e220b4c8c849487317708b0c1246b3451
Fedor Katurov <gotham48@gmail.com>
2019-12-10 12:07:06 +0700

    fixed models for nested fields

--- model/route.go
+++ model/route.go
@@ -1,6 +1,11 @@
 package model
 
-import "github.com/jinzhu/gorm"
+import (
+	"database/sql/driver"
+	"encoding/json"
+
+	"github.com/jinzhu/gorm"
+)
 
 type Point struct {
 	Lat float64
@@ -15,16 +20,17 @@ type Sticker struct {
 	Text    string
 }
 
+type PointArray []Point
+type StickerArray []Sticker
+
 type Route struct {
 	*gorm.Model
 
 	Address     string
 	Title       string
-	Version     int        `json:"-"`
-	RawRoute    string     `json:"-" sql:"route" gorm:"name:route;type:longtext"`
-	Route       []*Point   `sql:"-"`
-	RawStickers string     `json:"-" sql:"stickers" gorm:"name:stickers;type:longtext"`
-	Stickers    []*Sticker `sql:"-"`
+	Version     int          `json:"-"`
+	Route       PointArray   `sql:"route" gorm:"name:route;type:longtext"`
+	Stickers    StickerArray `gorm:"name:stickers;type:longtext"`
 	Distance    float64
 	IsPublic    bool
 	IsStarred   bool
@@ -34,4 +40,22 @@ type Route struct {
 	Description string
 	User        User `gorm:"foreignkey:UserId"`
 	UserId      uint
+}
+
+func (s *PointArray) Scan(src interface{}) error {
+	return json.Unmarshal(src.([]byte), &s)
+}
+
+func (s PointArray) Value() (driver.Value, error) {
+	val, err := json.Marshal(s)
+	return string(val), err
+}
+
+func (s *StickerArray) Scan(src interface{}) error {
+	return json.Unmarshal(src.([]byte), &s)
+}
+
+func (s StickerArray) Value() (driver.Value, error) {
+	val, err := json.Marshal(s)
+	return string(val), err
 }
