25dace113f2cda3bb5dcde8c346b248224326b1c
Fedor Katurov <gotham48@gmail.com>
2019-12-10 15:25:21 +0700

    error stack output

--- api/api.go
+++ api/api.go
@@ -1,16 +1,30 @@
 package api
 
 import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"runtime/debug"
+	"strings"
+	"time"
+
+	"github.com/NYTimes/gziphandler"
 	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
 
 	"github.com/muerwre/orchidgo/app"
-	"github.com/muerwre/orchidgo/utils/logger"
+	"github.com/muerwre/orchidgo/model"
 )
 
 type API struct {
 	App    *app.App
 	Config *Config
-	Logger *logger.Logger
+}
+
+type ErrorCode struct {
+	Code  string   `json:"code"`
+	Stack []string `json:"stack"`
 }
 
 func New(a *app.App) (api *API, err error) {
@@ -22,11 +36,105 @@ func New(a *app.App) (api *API, err error) {
 		return nil, err
 	}
 
-	api.Logger, _ = logger.CreateLogger(a)
-
 	return api, nil
 }
 
 func (a *API) Init(r *mux.Router) {
-	AuthRouter(r.PathPrefix("/auth").Subrouter(), a.Logger)
+	r.Use(gziphandler.GzipHandler, a.loggingMiddleware)
+
+	(&AuthRouter{}).Init(r.PathPrefix("/auth").Subrouter(), a)
+}
+
+type statusCodeRecorder struct {
+	http.ResponseWriter
+	http.Hijacker
+	StatusCode int
+}
+
+func (a *API) loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		r.Body = http.MaxBytesReader(w, r.Body, 100*1024*1024)
+
+		beginTime := time.Now()
+
+		hijacker, _ := w.(http.Hijacker)
+		w = &statusCodeRecorder{
+			ResponseWriter: w,
+			Hijacker:       hijacker,
+		}
+
+		ctx := a.App.NewContext().WithRemoteAddress(a.IPAddressForRequest(r, a.Config))
+		ctx = ctx.WithLogger(ctx.Logger.WithField("request_id", base64.RawURLEncoding.EncodeToString(model.NewId())))
+
+		defer func() {
+			statusCode := w.(*statusCodeRecorder).StatusCode
+			if statusCode == 0 {
+				statusCode = 200
+			}
+			duration := time.Since(beginTime)
+
+			logger := ctx.Logger.WithFields(logrus.Fields{
+				"duration":    duration,
+				"status_code": statusCode,
+				"remote":      ctx.RemoteAddress,
+			})
+			logger.Info(r.Method + " " + r.URL.RequestURI())
+		}()
+
+		w.Header().Set("Content-Type", "application/json")
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+// IPAddressForRequest determines IP Address for request
+func (a *API) IPAddressForRequest(r *http.Request, c *Config) string {
+	addr := r.RemoteAddr
+
+	if c.ProxyCount > 0 {
+		h := r.Header.Get("X-Forwarded-For")
+
+		if h != "" {
+			clients := strings.Split(h, ",")
+
+			if c.ProxyCount > len(clients) {
+				addr = clients[0]
+			} else {
+				addr = clients[len(clients)-c.ProxyCount]
+			}
+		}
+	}
+
+	return strings.Split(strings.TrimSpace(addr), ":")[0]
+}
+
+func (a *API) Handler(f func(*app.Context, http.ResponseWriter, *http.Request) error) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := a.App.NewContext().WithRemoteAddress(a.IPAddressForRequest(r, a.Config))
+		ctx = ctx.WithLogger(ctx.Logger.WithField("request_id", base64.RawURLEncoding.EncodeToString(model.NewId())))
+
+		var stack string
+
+		if a.Config.Debug {
+			stack = string(debug.Stack())
+		}
+
+		defer func() {
+			if r := recover(); r != nil {
+				w.Header().Set("Content-Type", "application/json")
+				ctx.Logger.Error(fmt.Errorf("%v: %s", r, debug.Stack()))
+				w.WriteHeader(http.StatusInternalServerError)
+				_ = json.NewEncoder(w).Encode(&ErrorCode{Code: "internal", Stack: strings.Split(stack, "\n")})
+			}
+		}()
+
+		if err := f(ctx, w, r); err != nil {
+			ctx.Logger.Error(err)
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusInternalServerError)
+			_ = json.NewEncoder(w).Encode(&ErrorCode{Code: "internal", Stack: strings.Split(stack, "\n")})
+			return
+		}
+	})
 }
--- api/auth.go
+++ api/auth.go
@@ -1,17 +1,18 @@
 package api
 
 import (
-	"github.com/NYTimes/gziphandler"
 	"github.com/gorilla/mux"
 	"github.com/muerwre/orchidgo/controller"
-	"github.com/muerwre/orchidgo/utils/logger"
 )
 
+type AuthRouter struct {
+}
+
 // AuthRouter for /api/auth/*
-func AuthRouter(router *mux.Router, logger *logger.Logger) {
+func (a *AuthRouter) Init(router *mux.Router, api *API) {
 	ctrl := &controller.AuthController{}
 
-	router.Handle("/", gziphandler.GzipHandler(logger.Log(ctrl.CheckCredentials))).Methods("GET")
-	router.Handle("/vk", gziphandler.GzipHandler(logger.Log(ctrl.LoginVkUser))).Methods("GET")
-	router.Handle("/guest", gziphandler.GzipHandler(logger.Log(ctrl.GetGuestUser))).Methods("GET")
+	router.Handle("/", api.Handler(ctrl.CheckCredentials)).Methods("GET")
+	router.Handle("/vk", api.Handler(ctrl.LoginVkUser)).Methods("GET")
+	router.Handle("/guest", api.Handler(ctrl.GetGuestUser)).Methods("GET")
 }
